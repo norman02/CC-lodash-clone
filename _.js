3e792e496effd7b4124a22013d79c424dec68797
John N. Norman <normanj777@gmail.com>
2022-01-20 14:40:22 -0500

    implement drop() method

--- _.js
+++ _.js
@@ -103,6 +103,15 @@ const _ = {
               return key
           }
       }
+  },
+  /**
+   * Creates a slice of `array` with `n` elements dropped from the beginning.
+   * @param {Array} array The array to query.
+   * @param {number} [n=1] The number of elements to drop.
+   * @returns {Array} Returns the slice of `array`
+   */
+  drop(array, n=1) {
+    return array.slice(n)
   }
 
 };
