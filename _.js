26f58494ea9b7ea45173f868f19038eb9f30d27a
John N. Norman <normanj777@gmail.com>
2022-01-20 09:18:56 -0500

    implement clamp function

--- _.js
+++ _.js
@@ -1,5 +1,20 @@
+/**
+ * Creates a `lodash` object.
+ */
 const _ = {
-    
+/**
+ * Clamps a number within the inclusive lower and upper bounds
+ * @name clamp
+ * @param number {number}: The number to clamp.
+ * @param lower {number}: The lower bound.
+ * @param upper {number}: The upper bound.
+ * 
+ * @returns {number}: returns the clamped number
+ */
+ clamp (number, lower, upper) {
+     return  Math.min(Math.max(number, lower), upper)
+    }
+
 }
 
 
