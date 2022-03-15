ce18525d1cf05045796ed096b419f0caa4aa1917
John N. Norman <normanj777@gmail.com>
2022-03-15 14:39:37 -0400

    implement the inRange function

--- _.js
+++ _.js
@@ -14,6 +14,21 @@ const _ = {
   clamp(number, lower, upper) {
     return Math.min(Math.max(number, lower), upper);
   },
+  /**
+   * Checks if `number` is between `start` and up to, but not including `end`. if `end` is not specifed, it is set to `start` with `start` then set to 0. if `start` is greater than end the params are swapped to support negative ranges.
+   * @name inRange
+   * @param {number} number
+   * @param {number} start
+   * @param {number} end
+   * @returns {boolean} true if number in range
+   */
+  inRange(number, start, end) {
+    if (end === undefined) {
+      end = start;
+      start = 0;
+    }
+    return number >= Math.min(start, end) && number < Math.max(start, end);
+  },
 };
 
 module.exports = _;
--- test.js
+++ test.js
@@ -2,13 +2,42 @@ const assert = require("assert");
 const _ = require("./_");
 
 describe("Clamp", () => {
+  let result = _.clamp(2, 1, 3);
+  let expected = 2;
   it("clamps a number in the range", () => {
-    assert.strictEqual(2, _.clamp(2, 1, 3));
+    assert.strictEqual(expected, result);
   });
   it("clamps values by the lower bound", () => {
-    assert.strictEqual(1, _.clamp(0, 1, 3));
+    result = _.clamp(0, 1, 3);
+    expected = 1;
+    assert.strictEqual(expected, result);
   });
   it("clamps the value by the upper bound", () => {
-    assert.strictEqual(3, _.clamp(5, 1, 3));
+    result = _.clamp(5, 1, 3);
+    expected = 3;
+    assert.strictEqual(expected, result);
+  });
+});
+
+describe("inRange", () => {
+  it("returns true if the number is in range", () => {
+    let result = _.inRange(3, 2, 4);
+    let expected = true;
+    assert.strictEqual(expected, result);
+  });
+  it("returns false if the number is not in range", () => {
+    let result = _.inRange(5, 2, 4);
+    let expected = false;
+    assert.strictEqual(expected, result);
+  });
+  it("assumes start = 0 if no input", () => {
+    let result = _.inRange(4, 8);
+    let expected = true;
+    assert.strictEqual(expected, result);
+  });
+  it("works with negative numbers", () => {
+    let result = _.inRange(-3, -2, -6);
+    let expected = true;
+    assert.strictEqual(expected, result);
   });
 });
