0928c7fb9714f1284b18ff6013cb8c0872dc7ecd
John N. Norman <normanj777@gmail.com>
2022-03-19 12:04:04 -0400

    refactor clamp method

--- _.js
+++ _.js
@@ -3,24 +3,7 @@
  */
 const _ = {
 
- 
-  
-  // sumBy TODO: implement sumBy method.
 
-  /** Number Methods */
-  /**
-   * Clamps a `number` within the inclusive `lower`
-   * and `upper` bounds
-   * @name clamp
-   * @param {number} number The `number` to clamp.
-   * @param {number} lower The `lower` bound.
-   * @param {number} upper The `upper` bound.
-   *
-   * @returns {number} returns the clamped `number`
-   */
-  clamp(number, lower, upper) {
-    return Math.min(Math.max(number, lower), upper);
-  },
   /**
    * Checks if `number` is between `start` and up to, but not
    * including `end`. if `end` is not specifed, it is set to
